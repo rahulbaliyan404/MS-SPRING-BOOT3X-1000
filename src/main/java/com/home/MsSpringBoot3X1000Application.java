08fc9bd121c9e70cb8bc03a271b20117b8ff67b9
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2024-12-25 08:28:48 +0530

    second commit

--- src/main/java/com/home/MsSpringBoot3X1000Application.java
+++ src/main/java/com/home/MsSpringBoot3X1000Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsSpringBoot3X1000Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsSpringBoot3X1000Application.class, args);
 	}
